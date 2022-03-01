f1c2b88f5323cfb51ea4f9d2c148c60cd586af04
zhpj <1220041602@qq.com>
2022-03-01 21:39:01 +0800

    sharding的配置及相关文档

--- frame-shardingjdbc/src/main/java/org/future/sharding/controller/UserController.java
+++ frame-shardingjdbc/src/main/java/org/future/sharding/controller/UserController.java
@@ -1,20 +1,37 @@
 package org.future.sharding.controller;
 
+import org.future.sharding.domain.TUser;
 import org.future.sharding.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 /**
  * @author: zhpj
  * @date: 2022-03-01 16:45
  */
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserMapper userMapper;
 
+    @GetMapping("/{id}")
+    public TUser getUserById(@PathVariable("id")Integer id) {
+        return userMapper.selectById(id);
+    }
 
+    @PostMapping("/insert")
+    @Transactional
+    public TUser insert(@Validated(value = TUser.InsertValid.class) TUser user) {
+        user.setPart(new Random().nextInt(10));
+        userMapper.insert(user);
+        return user;
+    }
 
 
 }
--- frame-shardingjdbc/src/main/java/org/future/sharding/domain/TUser.java
+++ frame-shardingjdbc/src/main/java/org/future/sharding/domain/TUser.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
 /**
  * @author: zhpj
  * @date: 2022-03-01 16:44
@@ -13,8 +16,15 @@ import lombok.Data;
 public class TUser {
 
     @TableId
-    private String cId;
+    @Null(groups = InsertValid.class)
+    private Long id;
 
+    @NotNull(groups = InsertValid.class, message = "cName字段不能为空")
     private String cName;
+
+//    @TableField(exist = false)
+    private Integer part;
+
+    public interface InsertValid {}
 
 }
